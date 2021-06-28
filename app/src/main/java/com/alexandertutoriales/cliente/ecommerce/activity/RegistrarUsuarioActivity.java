e91b8a5bc8ed4c9ee73b2c9535aa47a792920574
Alexander Fuentes Medina <alexanderfuentes199912@gmail.com>
2021-06-28 09:07:35 -0500

    Cambios realizado

---
+++ app/src/main/java/com/alexandertutoriales/cliente/ecommerce/activity/RegistrarUsuarioActivity.java
@@ -0,0 +1,16 @@
+package com.alexandertutoriales.cliente.ecommerce.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.alexandertutoriales.cliente.ecommerce.R;
+
+public class RegistrarUsuarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar_usuario);
+    }
+}
