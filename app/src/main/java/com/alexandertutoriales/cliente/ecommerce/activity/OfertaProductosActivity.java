0d893d8900bff67388df36d554e638dbfda71404
Wilson Hernande Neira Mija <WILSONHNM@ext.zara.com>
2023-11-04 11:10:51 -0500

    [Master] feat: Se corrigio la busqueda de productos en la clase OfertaProductosAdapter.

--- app/src/main/java/com/alexandertutoriales/cliente/ecommerce/activity/OfertaProductosActivity.java
+++ app/src/main/java/com/alexandertutoriales/cliente/ecommerce/activity/OfertaProductosActivity.java
@@ -3,34 +3,38 @@ package com.alexandertutoriales.cliente.ecommerce.activity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.alexandertutoriales.cliente.ecommerce.R;
 import com.alexandertutoriales.cliente.ecommerce.adapter.OfertaProductosAdapter;
 import com.alexandertutoriales.cliente.ecommerce.communication.Communication;
 import com.alexandertutoriales.cliente.ecommerce.communication.MostrarBadge;
 import com.alexandertutoriales.cliente.ecommerce.entity.service.DetallePedido;
-import com.alexandertutoriales.cliente.ecommerce.entity.service.OfertaProducto;
 import com.alexandertutoriales.cliente.ecommerce.utils.Carrito;
 import com.alexandertutoriales.cliente.ecommerce.viewmodel.OfertaProductoViewModel;
 import com.google.android.material.badge.BadgeDrawable;
 import com.google.android.material.badge.BadgeUtils;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import cn.pedant.SweetAlert.SweetAlertDialog;
 
 public class OfertaProductosActivity extends AppCompatActivity implements Communication, MostrarBadge {
     private OfertaProductoViewModel ofertaProductoViewModel;
     private OfertaProductosAdapter ofertaProductosAdapter;
-    private List<OfertaProducto> ofertaProductos = new ArrayList<>();
     private RecyclerView rcvOfertaProductos;
+    private SearchView searchView;
+    private BadgeDrawable badgeDrawable;
+    private SwipeRefreshLayout swipeOfertaProductosActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,17 +44,23 @@ public class OfertaProductosActivity extends AppCompatActivity implements Commun
         initViewModel();
         initAdapter();
         loadData();
+        filterProducts();
     }
 
     private void init() {
-        /*Toolbar toolbar = this.findViewById(R.id.toolbar);
+        Toolbar toolbar = this.findViewById(R.id.toolbar);
         toolbar.setNavigationIcon(R.drawable.ic_volver_atras);
         toolbar.setNavigationOnClickListener(v -> {//Reemplazo con lamba
             this.finish();
             this.overridePendingTransition(R.anim.rigth_in, R.anim.rigth_out);
-        });*/
+        });
         rcvOfertaProductos = findViewById(R.id.rcvOfertaProductos);
         rcvOfertaProductos.setLayoutManager(new GridLayoutManager(this, 2));
+        TypedValue tv = new TypedValue();
+        swipeOfertaProductosActivity = findViewById(R.id.swipeOfertaProductosActivity);
+        getApplicationContext().getTheme().resolveAttribute(R.color.pink_700, tv, false);
+        swipeOfertaProductosActivity.setColorSchemeColors(tv.data);
+        swipeOfertaProductosActivity.setOnRefreshListener(this::loadData);
     }
 
     private void initViewModel() {
@@ -59,14 +69,42 @@ public class OfertaProductosActivity extends AppCompatActivity implements Commun
     }
 
     private void initAdapter() {
-        ofertaProductosAdapter = new OfertaProductosAdapter(ofertaProductos, this, this);
+        ofertaProductosAdapter = new OfertaProductosAdapter(new ArrayList<>(), this, this);
         rcvOfertaProductos.setAdapter(ofertaProductosAdapter);
     }
 
     private void loadData() {
+        swipeOfertaProductosActivity.setRefreshing(true);
         int idOferta = getIntent().getIntExtra("idOferta", 0);//Recibimos el idOferta.
         ofertaProductoViewModel.listarOfertasProductos(idOferta).observe(this, response -> {
             ofertaProductosAdapter.updateItems(response.getBody());
+            swipeOfertaProductosActivity.setRefreshing(false);
+        });
+    }
+
+    private void filterProducts() {
+        searchView = findViewById(R.id.textinputSearch);
+        View closeButton = searchView.findViewById(androidx.appcompat.R.id.search_close_btn);
+
+        if (closeButton != null) {
+            closeButton.setOnClickListener(v -> {
+                ofertaProductosAdapter.getFilter().filter("");
+                searchView.setQuery("", false);
+            });
+        }
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String text) {
+                ofertaProductosAdapter.getFilter().filter(text);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String text) {
+                ofertaProductosAdapter.getFilter().filter(text);
+                return false;
+            }
         });
     }
 
--- app/src/main/java/com/alexandertutoriales/cliente/ecommerce/adapter/OfertaProductosAdapter.java
+++ app/src/main/java/com/alexandertutoriales/cliente/ecommerce/adapter/OfertaProductosAdapter.java
@@ -5,10 +5,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.alexandertutoriales.cliente.ecommerce.R;
@@ -29,13 +30,15 @@ import org.jetbrains.annotations.NotNull;
 
 import java.sql.Date;
 import java.sql.Time;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
-public class OfertaProductosAdapter extends RecyclerView.Adapter<OfertaProductosAdapter.ViewHolder> {
+public class OfertaProductosAdapter extends RecyclerView.Adapter<OfertaProductosAdapter.ViewHolder> implements Filterable {
     private final Communication communication;
     private final MostrarBadge mostrarBadgeComunication;
     private List<OfertaProducto> ofertaProductos;
+    private List<OfertaProducto> ofertaProductosFiltrados = new ArrayList<>();
 
     public OfertaProductosAdapter(List<OfertaProducto> ofertaProductos, Communication communication, MostrarBadge mostrarBadgeComunication) {
         this.ofertaProductos = ofertaProductos;
@@ -45,30 +48,70 @@ public class OfertaProductosAdapter extends RecyclerView.Adapter<OfertaProductos
 
     @NotNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(@NotNull ViewGroup parent, int viewType) {
         final View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_ofertas_productos, parent, false);
         return new ViewHolder(v);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull @NotNull ViewHolder holder, int position) {
-        holder.setItem(this.ofertaProductos.get(position));
+    public void onBindViewHolder(@NotNull ViewHolder holder, int position) {
+        holder.setItem(this.ofertaProductosFiltrados.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return this.ofertaProductos.size();
+        return this.ofertaProductosFiltrados.size();
     }
 
-    public void updateItems(List<OfertaProducto> platillo) {
+    public void updateItems(List<OfertaProducto> producto) {
         this.ofertaProductos.clear();
-        this.ofertaProductos.addAll(platillo);
+        this.ofertaProductos.addAll(producto);
+
+        this.ofertaProductosFiltrados.clear();
+        this.ofertaProductosFiltrados.addAll(producto);
         this.notifyDataSetChanged();
+    }
+
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                String charSearch = constraint != null ? constraint.toString() : "";
+                List<OfertaProducto> resultList;
+
+                if (charSearch.isEmpty()) {
+                    ofertaProductosFiltrados = ofertaProductos;
+                } else {
+                    resultList = new ArrayList<>();
+                    for (OfertaProducto p : ofertaProductos) {
+                        if (p.getIdPlatillo().getNombre().toLowerCase(Locale.ROOT).contains(charSearch.toLowerCase(Locale.ROOT))) {
+                            resultList.add(p);
+                        }
+                    }
+                    ofertaProductosFiltrados = resultList;
+                }
+
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = ofertaProductosFiltrados;
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+                if (results != null && results.values != null) {
+                    ofertaProductosFiltrados = (ArrayList<OfertaProducto>) results.values;
+                } else {
+                    ofertaProductosFiltrados = new ArrayList<>();
+                }
+                notifyDataSetChanged();
+            }
+        };
     }
 
     protected class ViewHolder extends RecyclerView.ViewHolder {
 
-        public ViewHolder(@NonNull @NotNull View itemView) {
+        public ViewHolder(@NotNull View itemView) {
             super(itemView);
         }
 
@@ -91,7 +134,8 @@ public class OfertaProductosAdapter extends RecyclerView.Adapter<OfertaProductos
                     .into(imgProductoOferta);
             tvProductoOferta.setText(p.getIdPlatillo().getNombre());
             tvPrecioOferta.setText(String.format(Locale.ENGLISH, "S/%.2f", p.getPrecioAhora()));
-            tvDescuento.setText(Integer.toString(p.getDescuento()).concat("%"));
+            //tvDescuento.setText(Integer.toString(p.getDescuento()).concat("%"));
+            tvDescuento.setText(String.format(Locale.ENGLISH, "%d%%", p.getDescuento()));
             tvPrecioAntes.setText(String.format(Locale.ENGLISH, "S/%.2f", p.getIdPlatillo().getPrecio()));
 
             btnComprarProductoOferta.setOnClickListener(v -> {
@@ -101,14 +145,14 @@ public class OfertaProductosAdapter extends RecyclerView.Adapter<OfertaProductos
                 detallePedido.setPrecio(p.getPrecioAhora());
                 mostrarBadgeComunication.add(detallePedido);
             });
-            //Cuando pulses sobre la tarjeta te aparecerá el detalle de ese platillo
+            //Cuando pulses sobre la tarjeta te aparecerá el detalle de ese producto
             itemView.setOnClickListener(v -> {
                 final Intent i = new Intent(itemView.getContext(), DetallePlatilloActivity.class);
                 final Gson g = new GsonBuilder()
                         .registerTypeAdapter(Date.class, new DateSerializer())
                         .registerTypeAdapter(Time.class, new TimeSerializer())
                         .create();
-                i.putExtra("detallePlatillo", g.toJson(p.getIdPlatillo()));
+                i.putExtra("detalleproducto", g.toJson(p.getIdPlatillo()));
                 communication.showDetails(i);//Esto es solo para dar una animación.
             });
         }
